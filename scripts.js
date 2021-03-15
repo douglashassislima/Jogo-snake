d63801d9dee2afd6a223f4be07ee69948a134bc6
douglashassislima <douglashenriquedeassis@gmail.com>
2021-03-15 20:48:15 -0300

    adicionando nome ao jogo

--- scripts.js
+++ scripts.js
@@ -41,6 +41,15 @@ function update(event) {
 }
 
 function iniciarJogo() {
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game Over!");
+        }
+    }
+
+
     if (snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -58,8 +67,12 @@ function iniciarJogo() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
-
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
     let newHead = {
         x: snakeX,
         y: snakeY
